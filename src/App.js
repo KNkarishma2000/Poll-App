5a1d5d56abaa3b6721e124861d76de4ae27842db
Karishma Khatri <114894267+KNkarishma2000@users.noreply.github.com>
2026-05-08 17:43:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import Home from './Home';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import Test from "./Test";
 
 function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/test" element={<Test />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
